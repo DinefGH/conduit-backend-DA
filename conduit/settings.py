d9e34c4206ae4e926952f8e809273e33a5c2ae56
DinefGH <112215706+DinefGH@users.noreply.github.com>
2025-07-24 22:58:38 +0200

    add whitenoise==4.1.4 to requirements

--- conduit/settings.py
+++ conduit/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,6 +131,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_WHITELIST = (
     os.environ.get('ALLOWED_ORIGIN', 'http://localhost:8282'),
