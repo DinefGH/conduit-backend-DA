d48ae009bce64c5368b8c865d09af8a090695413
DinefGH <112215706+DinefGH@users.noreply.github.com>
2025-07-25 18:26:25 +0200

    change articles url

--- conduit/apps/articles/urls.py
+++ conduit/apps/articles/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import include, url
-
 from rest_framework.routers import DefaultRouter
 
 from .views import (
@@ -7,22 +6,23 @@ from .views import (
     CommentsListCreateAPIView, CommentsDestroyAPIView, TagListAPIView
 )
 
-router = DefaultRouter(trailing_slash=False)
+# Allow both slash‑terminated AND non‑slash URLs on the ViewSet routes
+router = DefaultRouter(trailing_slash='/?')
 router.register(r'articles', ArticleViewSet)
 
 urlpatterns = [
+    # this now serves:
+    #   GET/POST  /articles       and  /articles/
+    #   GET       /articles/{pk}  and  /articles/{pk}/
     url(r'^', include(router.urls)),
 
+    # feed, favorites, comments and tags all accept an optional slash too
     url(r'^articles/feed/?$', ArticlesFeedAPIView.as_view()),
-
     url(r'^articles/(?P<article_slug>[-\w]+)/favorite/?$',
         ArticlesFavoriteAPIView.as_view()),
-
-    url(r'^articles/(?P<article_slug>[-\w]+)/comments/?$', 
+    url(r'^articles/(?P<article_slug>[-\w]+)/comments/?$',
         CommentsListCreateAPIView.as_view()),
-
     url(r'^articles/(?P<article_slug>[-\w]+)/comments/(?P<comment_pk>[\d]+)/?$',
         CommentsDestroyAPIView.as_view()),
-
     url(r'^tags/?$', TagListAPIView.as_view()),
 ]
